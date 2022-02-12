b72f5b5af9677180b02b37f6241542bf35a66ef0
zhubiaook <zhubiaook@outlook.com>
2022-02-12 13:47:23 +0800

    feat: add color.sh

---
+++ scripts/lib/color.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+red_echo ()      { echo -e "\033[031;1m$@\033[0m"; }
+green_echo ()    { echo -e "\033[032;1m$@\033[0m"; }
+yellow_echo ()   { echo -e "\033[033;1m$@\033[0m"; }
+blue_echo ()     { echo -e "\033[034;1m$@\033[0m"; }
+purple_echo ()   { echo -e "\033[035;1m$@\033[0m"; }
+bred_echo ()     { echo -e "\033[041;1m$@\033[0m"; }
+bgreen_echo ()   { echo -e "\033[042;1m$@\033[0m"; }
+byellow_echo ()  { echo -e "\033[043;1m$@\033[0m"; }
+bblue_echo ()    { echo -e "\033[044;1m$@\033[0m"; }
+bpurple_echo ()  { echo -e "\033[045;1m$@\033[0m"; }
